94ebb87a1b398575ed416be898c342c8493e1530
Anand Bagmar <abagmar@gmail.com>
2014-02-27 12:12:43 +0530

    Anand - remove Cucumber options from RunCukesTest.

---
+++ src/test/java/gradle/cucumber/RunCukesTest.java
@@ -0,0 +1,9 @@
+package gradle.cucumber;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class RunCukesTest {
+
+}
--- src/test/java/gradle/cucumber/steps/BasicStepdefs.java
+++ src/test/java/gradle/cucumber/steps/BasicStepdefs.java
@@ -1,9 +1,12 @@
-package gradle.cucumber;
+package gradle.cucumber.steps;
 
 import cucumber.api.java.en.*;
+import gradle.cucumber.BaseStepdefs;
+import gradle.cucumber.Production;
+
 import java.lang.Throwable;
 
-public class BasicStepdefs extends BaseStepdefs{
+public class BasicStepdefs extends BaseStepdefs {
 
     @When("^I run a failing step")
     public void I_run_a_failing_step() throws Throwable {
