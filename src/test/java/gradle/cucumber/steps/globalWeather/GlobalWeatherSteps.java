f8d802c25e8e2a0192887201b70610179be96e5b
Anand Bagmar <abagmar@gmail.com>
2014-03-05 12:20:29 +0530

    Anand - remove unused (temporary) step

--- src/test/java/gradle/cucumber/steps/globalWeather/GlobalWeatherSteps.java
+++ src/test/java/gradle/cucumber/steps/globalWeather/GlobalWeatherSteps.java
@@ -12,22 +12,9 @@ import javax.xml.namespace.QName;
 import java.net.URL;
 
 public class GlobalWeatherSteps {
-    @Given("^I get list of cities$")
-    public void I_get_list_of_cities() throws Throwable {
-        GlobalWeather globalWeather = new GlobalWeather();
-        String cities = globalWeather.getGlobalWeatherSoap().getCitiesByCountry("India");
-        String weather = globalWeather.getGlobalWeatherSoap().getWeather("Pune", "India");
-        System.out.println("Cities: " + cities);
-
-        CurrencyConvertor currencyConvertor = new CurrencyConvertor();
-        double rate = currencyConvertor.getCurrencyConvertorSoap().conversionRate(Currency.AUD, Currency.INR);
-        System.out.println("\n\nRate: " + rate);
-    }
-
     @When("^I get list of cities for \"(.*)\"$")
     public void I_get_list_of_cities_for_country(String forCountry) throws Throwable {
         WeatherFlow weatherFlow = new WeatherFlow();
         weatherFlow.getCities(forCountry);
-
     }
 }
