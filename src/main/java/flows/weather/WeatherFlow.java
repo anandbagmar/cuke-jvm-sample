4505fc83e8bfb6b112236a6fc627abaf3cfa47fc
Anand Bagmar <abagmar@gmail.com>
2014-03-05 15:31:38 +0530

    Anand - completed 1 scenario for GlobalWeather webservice
    Conflicts:
    	src/main/java/flows/weather/WeatherFlow.java
    	src/test/java/gradle/cucumber/steps/globalWeather/GlobalWeatherSteps.java
    	src/test/resources/weather/weather.feature

--- src/main/java/flows/weather/WeatherFlow.java
+++ src/main/java/flows/weather/WeatherFlow.java
@@ -6,6 +6,11 @@ import flows.webserviceClients.globalWeather.net.webservices.GlobalWeatherSoap;
 
 import javax.xml.ws.BindingProvider;
 import java.util.Map;
+import com.jayway.jsonpath.JsonPath;
+import utils.FileUtils;
+import utils.RuntimeUtils;
+
+import java.util.List;
 
 public class WeatherFlow {
 
@@ -30,9 +35,21 @@ public class WeatherFlow {
 
     }
 
-    public String getCities(String forCountry) {
-        String cities = this.weatherService.getCitiesByCountry(forCountry);
-        System.out.format("Cities for " + forCountry + ": \n$" + cities);
-        return cities;
+    public List<String> getCities(String forCountry) {
+        String citiesByCountry = this.weatherService.getCitiesByCountry(forCountry);
+        String jsonString = FileUtils.convertStringToJsonString(citiesByCountry);
+        List<String> listOfCities = JsonPath.read(jsonString, "$.NewDataSet.Table[*].City");
+        System.out.println("\tNumber of Cities: " + listOfCities.size());
+        RuntimeUtils.saveState("listOfCitiesFor" + forCountry, listOfCities);
+        return listOfCities;
+    }
+
+    public void getWeatherForCities(int numberOfCities, String forCountry) {
+        List<String> listOfCities = (List<String>) RuntimeUtils.retrieveState("listOfCitiesFor" + forCountry);
+        for(int cityNumber=0; cityNumber<numberOfCities; cityNumber++) {
+            String cityName = listOfCities.get(cityNumber);
+            String weather = this.weatherService.getWeather(cityName, forCountry);
+            System.out.println ("\n\tWeather for city: " + cityName + "::" + forCountry + ": " + weather);
+        }
     }
 }
