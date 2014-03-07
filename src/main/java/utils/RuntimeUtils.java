4b2d8e91a0b94896e531bc7c2e7d20ff5f3ae15b
aasawaree <ritu.21090@gmail.com>
2014-03-07 16:37:20 +0530

    minor refactoring

--- src/main/java/utils/RuntimeUtils.java
+++ src/main/java/utils/RuntimeUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class RuntimeUtils {
     private static HashMap<String, Object> state = new HashMap<String, Object>();
@@ -12,5 +13,12 @@ public class RuntimeUtils {
 
     public static Object retrieveState(String key) {
         return state.get(key);
+    }
+
+    public static String loadfile(Map<String, String> entity){
+        return entity.get("entity_type");
+    }
+    public static String retrieveEntitySection(Map<String, String> entity){
+        return entity.get("data_section_to_use");
     }
 }
--- src/test/java/gradle/cucumber/steps/CreateCustomer.java
+++ src/test/java/gradle/cucumber/steps/CreateCustomer.java
@@ -6,14 +6,18 @@ import cucumber.api.DataTable;
 import java.util.List;
 import java.util.Map;
 
+import static utils.FileUtils.loadJsonSectionFrom;
+import static utils.RuntimeUtils.retrieveEntitySection;
+import static utils.RuntimeUtils.loadfile;
+
 public class CreateCustomer {
     @Given("^I create a new Individual Customer$")
     public void I_create_a_new_Individual_Customer(DataTable entities) throws Throwable {
         List<Map<String,String>> entitiesToUse = entities.asMaps();
-        for (int numberOfEntities = 0; numberOfEntities<entitiesToUse.size(); numberOfEntities++) {
-            Map<String, String> eachEntity = entitiesToUse.get(numberOfEntities);
-            for (String key : eachEntity.keySet()) {
-                System.out.println("key: " + key + ", value: " + eachEntity.get(key));
+        for(Map<String,String> eachEntity: entitiesToUse){
+            for(String key: eachEntity.keySet()){
+                System.out.println("key: " + key + " , value: " + eachEntity.get(key));
+                loadJsonSectionFrom(loadfile(eachEntity), retrieveEntitySection(eachEntity));
             }
         }
     }
