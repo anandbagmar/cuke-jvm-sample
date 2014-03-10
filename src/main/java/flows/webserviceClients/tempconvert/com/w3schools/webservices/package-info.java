3059151bd692edce769afb9373d9971f21be16f8
Rajdeep <mail.rajvarma@gmail.com>
2014-03-10 12:32:12 +0530

    Revert "[Rajdeep]: Adding a sample test for converting temperature"
    This reverts commit dada34290370354c82059437089ff12435603ac0.

--- src/main/java/flows/webserviceClients/tempconvert/com/w3schools/webservices/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3schools.com/webservices/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package flows.WebserviceClients.tempconvert.com.w3schools.webservices;
--- src/test/resources/temperature/temperature.feature
+++
@@ -1,5 +0,0 @@
-Feature: Temperature convert using web service
-
-  Scenario: Temperature convert from Celsius To Fahrenheit
-    When I convert "14" degree Fahrenheit to Celsius
-    Then I should get result as "-10"
