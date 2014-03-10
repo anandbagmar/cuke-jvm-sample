770857994f15a0460695bc80f8dff9a2456b931c
Anand Bagmar <abagmar@gmail.com>
2014-03-10 13:43:11 +0530

    Anand/Atirek - moved the logic of loading the entity files, retrieving specific json sections into EntityUtils.

--- src/main/java/utils/EntityUtils.java
+++ src/main/java/utils/EntityUtils.java
@@ -1,13 +1,16 @@
 package utils;
 
 import cucumber.api.DataTable;
+import gherkin.deps.com.google.gson.internal.StringMap;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static utils.FileUtils.loadJsonEntityFrom;
+
 public class EntityUtils {
-    public static HashMap<String, String> entitiesToBeLoaded(DataTable entities) {
+    private static HashMap<String, String> getListofJsonEntitiesToBeLoadedFrom(DataTable entities) {
         List<Map<String,String>> entitiesToUse = entities.asMaps();
         HashMap<String, String> loadEntities = new HashMap<String, String>();
         for(Map<String,String> eachEntity: entitiesToUse){
@@ -16,4 +19,20 @@ public class EntityUtils {
         return loadEntities;
     }
 
+    private static HashMap<String, StringMap> loadSpecifcSectionsFromJson(HashMap<String, String> loadEntities) {
+        HashMap<String, StringMap> loadedTestData = new HashMap<String, StringMap>();
+        for (String jsonFile : loadEntities.keySet()) {
+            StringMap loadedJson = (StringMap) loadJsonEntityFrom(jsonFile, loadEntities.get(jsonFile));
+            loadedTestData.put(loadEntities.get(jsonFile), loadedJson);
+//            for (Object key : loadedJson.keySet()) {
+//                System.out.println("\t" + key.toString() + " : " + loadedJson.get(key));
+//            }
+        }
+        return loadedTestData;
+    }
+
+    public static HashMap<String, StringMap>  getTestDataFromEntities(DataTable entities) {
+        HashMap<String, String> loadEntities = EntityUtils.getListofJsonEntitiesToBeLoadedFrom(entities);
+        return EntityUtils.loadSpecifcSectionsFromJson(loadEntities);
+    }
 }
--- src/test/java/gradle/cucumber/steps/CreateCustomer.java
+++ src/test/java/gradle/cucumber/steps/CreateCustomer.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.DataTable;
 import gherkin.deps.com.google.gson.internal.StringMap;
 import utils.EntityUtils;
+
 import java.util.HashMap;
 
 import static utils.FileUtils.loadJsonEntityFrom;
@@ -11,13 +12,10 @@ import static utils.FileUtils.loadJsonEntityFrom;
 public class CreateCustomer {
     @Given("^I create a new Individual Customer$")
     public void I_create_a_new_Individual_Customer(DataTable entities) throws Throwable {
-        HashMap<String, String> loadEntities = EntityUtils.entitiesToBeLoaded(entities);
-        for (String jsonFile: loadEntities.keySet()) {
-            StringMap loadedJson = (StringMap) loadJsonEntityFrom(jsonFile, loadEntities.get(jsonFile));
-            System.out.println("loadedJson: " + loadedJson.size());
-            for (Object key: loadedJson.keySet()) {
-                System.out.println ("\t" + key.toString() + " : " + loadedJson.get(key));
-            }
-       }
+        HashMap<String, StringMap> loadedTestData = EntityUtils.getTestDataFromEntities(entities);
+        System.out.println("Loaded Test Data: ");
+        for (Object key : loadedTestData.keySet()) {
+            System.out.println("\t" + key.toString() + " : " + loadedTestData.get(key));
+        }
     }
 }
